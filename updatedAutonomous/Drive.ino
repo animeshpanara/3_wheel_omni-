cced0159c38062cb099c3fd8494780f2d1c6bf93
animeshpanara <animeshpanara30@gmail.com>
2018-01-13 11:31:58 +0530

    final integration with pi done
    for camera color sensing of ball

--- updatedAutonomous/Drive.ino
+++ updatedAutonomous/Drive.ino
@@ -1,3 +1,7 @@
+void initUltraSonicSensor(){
+  pinMode (TriggerPin, INPUT); 
+  pinMode (EchoPin, INPUT);
+}
 
 void initThrowing(){
   pinMode(ThrowPin,OUTPUT);
--- updatedAutonomous/throwControls.ino
+++ updatedAutonomous/throwControls.ino
@@ -6,14 +6,39 @@ void ThrowShuttleCock(){
    delay(1000);
 }
 
+void chechIr(){
+  while(1){
+    if(!digitalRead(TriggerPin))
+      Ircounter++;
+    else
+      Ircounter=0;
+    Serial.println("IRCounter:"+String(Ircounter));  
+    if(Ircounter>50)
+    {
+      Ircounter=0;
+      break;
+    }
+  }
+}
+
+int CheckBall(){
+  PiSerial.write('a');
+  while(PiSerial.available()<=0);
+  char x=PiSerial.read();
+  Serial.println("Ball detected:"+String(x));
+  if(x=='a') return 5;
+  else if(x=='b') return 4;
+  else if(x=='r') return 3;
+  else return 0;
+}
 void LoadBot(){
    int LoadPos;
    if(pos[1]==3)
        LoadPos=1;
    else if(pos[1]==5){
-       LoadPos=0;
+       LoadPos=1;
        DACcounter=0;
-       dac.setVoltage(DACcounter, true);         
+       dac.setVoltage(DACcounter,false);         
    }    
    else
        LoadPos=2;
